f2d750948612540262d19bbcd0cc7c1db5bd7c88
Marina <marina.slepinina3@gmail.com>
2025-11-23 17:00:41 +0200

    update app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Header } from "@/components/Header/Header";
+import { Hero } from "@/components/Hero/Hero";
+import { Deals } from "@/components/Deals/Deals";
+import { Numbers } from "@/components/Numbers/Numbers";
 
 export default function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+        <Numbers />
+        <Deals />
+      </main>
     </>
   );
 }
