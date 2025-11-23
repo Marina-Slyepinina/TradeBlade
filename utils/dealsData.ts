ffabb6d614761823375a94cdd92892bdc62bb014
Marina <marina.slepinina3@gmail.com>
2025-11-23 16:49:04 +0200

    added svg into sprite, updated global.css

---
+++ utils/dealsData.ts
@@ -0,0 +1,37 @@
+export const deals = [
+  {
+    pair: 'NEAR/USDT',
+    time: 'SPOT - 1 хв. назад',
+    profit: '58.6206%',
+    target: 'Ціль 4',
+    date: '06.10.2022',
+  },
+  {
+    pair: 'BTC/USDT',
+    time: 'SPOT - 1 хв. назад',
+    profit: '6.02%',
+    target: 'Ціль 3',
+    date: '06.10.2022',
+  },
+  {
+    pair: 'ETH/USDT',
+    time: 'SPOT - 1 хв. назад',
+    profit: '16.3%',
+    target: 'Ціль 4',
+    date: '06.10.2022',
+  },
+  {
+    pair: 'NEAR/USDT',
+    time: 'SPOT - 1 хв. назад',
+    profit: '0.963%',
+    target: 'Ціль 4',
+    date: '06.10.2022',
+  },
+  {
+    pair: 'NEAR/USDT',
+    time: 'SPOT - 1 хв. назад',
+    profit: '58.6206%',
+    target: 'Ціль 4',
+    date: '06.10.2022',
+  },
+];
